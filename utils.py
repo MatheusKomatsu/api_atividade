0f3218bee8f14ea42af61c90bdd7c4a7e0360b2e
Matheus Eiji Faria Komatsu <matheuskomatsu@hotmail.com>
2024-03-23 19:34:22 -0300

    Added autentication with simple database using a table of users with passwords. Included flask-httpauth to the requirements

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 
 def insere_pessoas():
-    pessoa = Pessoas(nome='Rafael', idade=29)
-    pessoa.save()
+    pessoa1 = Pessoas(nome='Rafael', idade=29)
+    pessoa2 = Pessoas(nome='Matheus', idade=21)
+    pessoa3 = Pessoas(nome='Gabriel', idade=19)
+    pessoa1.save()
+    pessoa2.save()
+    pessoa3.save()
 def consulta_pessoas():
     pessoa = Pessoas.query.all()
     for i in pessoa:
@@ -18,10 +22,21 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
+def insere_usuario(login,senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
-    insere_pessoas()
-    consulta_pessoas()
-    altera_pessoas()
-    consulta_pessoas()
-    exclui_pessoa()
-    consulta_pessoas()
+    insere_usuario('Matheus', '1234')
+    insere_usuario('Gabriel', '4321')
+    consulta_usuarios()
+    # insere_pessoas()
+    # consulta_pessoas()
+    # altera_pessoas()
+    # consulta_pessoas()
+    # exclui_pessoa()
+    # consulta_pessoas()
